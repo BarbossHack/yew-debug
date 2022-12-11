3215dec5391b1a556d85d37dd91a99dbbf0b553d
BarbossHack <barbossh4ck@protonmail.com>
2022-12-11 21:40:28 +0100

    register the worker

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 mod agent;
 mod app;
 
+use agent::Agent;
 use wasm_bindgen::prelude::*;
+use yew_agent::PublicWorker;
 
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
+    Agent::register();
     yew::Renderer::<app::App>::new().render();
     Ok(())
 }
